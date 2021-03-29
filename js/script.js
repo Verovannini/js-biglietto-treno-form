dac2c11faa70eb1f207c3e5d8c5f11e398f15c79
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-03-29 16:57:39 +0200

    Add name and offer

--- js/script.js
+++ js/script.js
@@ -6,3 +6,35 @@
 // Categoria selezionata dall'utente
 // Se clicchiamo su Genera, la sezione col biglietto apparirà e sarà popolata coi dati del biglietto
 // Se clicchiamo su annulla dobbiamo ripulire il form e nascondere la sezione col biglietto.
+
+// Funzioni generateButtonElement
+// Quando si fa click su generate button comprare il div con il biglietto
+var generateButtonElement = document.getElementById('generate-button');
+generateButtonElement.addEventListener('click', function() {
+    // Far apparire il div quando si fa click
+    var ticketVisibleElement = document.getElementById('ticket-visible');
+    ticketVisibleElement.className = ticketVisibleElement.classList + ' visible';
+
+    // Prendere il nome dalla value dell'input ticket-generator-name e stamparla in ticket-user-name
+    var ticketGeneratorNameElement = document.getElementById('ticket-generator-name');
+    var userName = ticketGeneratorNameElement.value;
+    console.log(userName);
+
+    document.getElementById('ticket-user-name').innerHTML = userName;
+
+    // Prendere la categoria del biglietto dal value del select ticket-generator-age e stamparla in ticket-user-offer
+    var ticketGeneratorAgeElement = document.getElementById('ticket-generator-age');
+    var userOfferValue = ticketGeneratorAgeElement.value;
+    console.log(userOfferValue);
+
+    var userOffer = 'Prezzo base';
+    if(userOfferValue == 'minorenne') {
+        userOffer = 'Sconto Minorenni';
+    } else if ( userOfferValue == 'over' ) {
+        userOffer = 'Sconto Silver';
+    }
+    console.log(userOffer);
+
+    document.getElementById('ticket-user-offer').innerHTML = userOffer;
+
+});
